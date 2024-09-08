411ee0993f830a62a4310492ceaf35d97e8453e7
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-09-08 20:14:26 -0300

    feat(#1999): Building basic structure

---
+++ source/Domain/CloudSuite.OpenAI.Modules.Domain/Models/Prompt.cs
@@ -0,0 +1,26 @@
+﻿using NetDevPack.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.OpenAI.Modules.Domain.Models
+{
+    public class Prompt : Entity, IAggregateRoot
+    {
+        public Prompt(string? text, int? maxTokens, DateTime? timestamp)
+        {
+            Text = text;
+            MaxTokens = maxTokens;
+            Timestamp = DateTime.Now;
+        }
+
+        public string? Text { get; private set; }
+
+        public int? MaxTokens { get; private set; }
+
+        public DateTime? Timestamp { get; private set; }
+    }
+}
